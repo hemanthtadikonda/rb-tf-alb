8ef61e3aec2735827922fb5f1ad9f04c3286f147
hemanth <hemanthtadikonda29@gmail.com>
2024-04-04 10:49:18 +0530

    ACM configuration

--- main.tf
+++ main.tf
@@ -66,12 +66,12 @@ resource "aws_lb_listener" "frontend" {
   }
 }
 
-#resource "aws_security_group_rule" "frontend" {
-#  count             = var.internal ? 0 : 1
-#  type              = "ingress"
-#  from_port         = 80
-#  to_port           = 80
-#  protocol          = "tcp"
-#  cidr_blocks       = var.sg_ingress_cidr
-#  security_group_id = aws_security_group.main.id
-#}
+resource "aws_security_group_rule" "frontend" {
+  count             = var.internal ? 0 : 1
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = var.sg_ingress_cidr
+  security_group_id = aws_security_group.main.id
+}
